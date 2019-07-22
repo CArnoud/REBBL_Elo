e0375273735ac5c9860317749b21d2b2889c3bd1
charles-arnoud <c.arnoud@hotmail.com>
2019-07-22 20:14:48 -0300

    Updated matchup string.

--- seeMatchups.js
+++ seeMatchups.js
@@ -29,21 +29,27 @@ for (let i in games) {
         getTeamString(teams[0], teams[1]) +
         'VS\n' +
         getTeamString(teams[1], teams[0]) +
+        getRaceMatchupString(teams) +
         'Winner: ' + currentGame.winner_id +
         '\n'
     );
 }
 
+function getRaceMatchupString(teams) {
+    const raceIndex = teams[0].race > teams[1].race ? teams[1].race + teams[0].race : teams[0].race + teams[1].race;
+    return matchups[raceIndex][teams[0].race] + ' ' + teams[0].race + ' - ' +
+        matchups[raceIndex][teams[1].race] + ' ' + teams[1].race + ' - ' +
+        matchups[raceIndex]['draw'] + ' draw\n';
+}
+
 function getTeamString(team, opponent) {
     const elo = eloCalculator.getTeamElo(team.id);
     const oppElo = eloCalculator.getTeamElo(opponent.id);
-    const raceIndex = team.race > opponent.race ? opponent.race + team.race : team.race + opponent.race;
 
     return team.id + ' - ' + 
         team.tv.toString().padStart(4) + 
         ' TV ' + team.race.padEnd(12, ' ') + 
         ' (' + Math.round(elo) + ' Elo): ' + 
-        (eloCalculator.getExpectedResult(elo, oppElo)*100).toFixed(2) + '%\n' + 
-        matchups[raceIndex][team.race] + '-' + matchups[raceIndex]['draw'] + '-' + matchups[raceIndex][opponent.race] + ' vs ' + opponent.race +
+        (eloCalculator.getExpectedResult(elo, oppElo)*100).toFixed(2) + '%' + 
         '\n';
 }
