0c34b1e27777fd078aa231ff6c3eeea7f3a21d61
charles-arnoud <c.arnoud@hotmail.com>
2019-08-24 23:22:55 -0300

    Pick results finished.

--- predictions/getResults.js
+++ predictions/getResults.js
@@ -21,10 +21,17 @@ const fileNames = fileHelper.readDir(directoryPath);
 // Load predictions
 const predictions = JSON.parse(fileHelper.readFile(config.FILE.predictionFileName));
 
-let numberOfGames = 0;
+let totalGames = 0;
+let totalCorrect = 0;
+let totalDraws = 0;
+let gamesToBePlayed = 0;
+
 for (let i=0; i < fileNames.length; i++) {
     const rounds = JSON.parse(fileHelper.readFile(directoryPath + '/' + fileNames[i]));
     const games = rounds[roundIndex];
+    let numberOfGames = 0;
+    let correctPicks = 0;
+    gamesToBePlayed = gamesToBePlayed + games.length;
 
     // Proccess games
     for (let j in games) {
@@ -32,10 +39,32 @@ for (let i=0; i < fileNames.length; i++) {
         const teams = currentGame.getTeams();
 
         if (currentGame.match_id) {
+            if (currentGame.getWinnerId() === getFavoriteId(teams)) {
+                correctPicks++;
+            }
+
+            if (!currentGame.getWinnerId()) {
+                totalDraws++;
+            }
+
             numberOfGames++;
-        }        
+        }
     }
 
-    console.log(numberOfGames + ' - ' + games.length);
+    console.log(fileNames[i]);
+    console.log(correctPicks + ' out of ' + numberOfGames + '\n');
+    totalGames = totalGames + numberOfGames;
+    totalCorrect = totalCorrect + correctPicks;
     numberOfGames = 0;
+    correctPicks = 0;
+}
+
+console.log('Total: \n' + totalCorrect + ' out of ' + totalGames + ' (' + (totalCorrect/totalGames).toFixed(4) + ')');
+console.log(totalCorrect + ' out of ' + (totalGames - totalDraws) + ' without draws (' + (totalCorrect/(totalGames-totalDraws)).toFixed(4) + ')');
+console.log((gamesToBePlayed - totalGames) + ' left to be played');
+
+function getFavoriteId(teams) {
+    const elo1 = eloCalculator.getTeamElo(teams[0].id);
+    const elo2 = eloCalculator.getTeamElo(teams[1].id);
+    return elo1 > elo2 ? teams[0].id : teams[1].id;
 }
